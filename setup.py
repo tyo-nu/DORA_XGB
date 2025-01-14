c32a747a71bd051a1d37de1f8d99715a554641c7
yashchainani21 <yashchainani2026@u.northwestern.edu>
2025-01-14 13:27:04 -0600

    updated version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def parse_requirements(filename):
 setup(
     name='DORA_XGB',
 
-    version='1.01',
+    version='1.2',
 
     description = "Gradient-booseted classifiers to predict the feasibility of enzymatic reactions",
                 
